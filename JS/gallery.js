f3a2d17123b5f34ec60e50b93d7300b67cdc42f7
crow0085 <crow0085@algonquinlive.com>
2022-11-12 06:07:36 -0500

    gallery.js: populates divs with images and names from the json

--- JS/gallery.js
+++ JS/gallery.js
@@ -3,11 +3,28 @@ var path = '/DATA/people.json';
 
 $(document).ready(async function () {
     peopleJson = await getJSON();
-    console.log(peopleJson);
+
+    // loop to populate gallery
+    peopleJson.people.forEach(function (value, i) {
+        var div = $('<div id="div' + i + '">');
+        var img = $('<img id="img' + i + '">');
+        img.attr('src', value.image.path);
+        div.append(img);
+        var name = $('<p id="name' + i + '">');
+        name.html(value.name);
+        div.append(name);
+        div.click(function () {
+            alert(this.id);
+        });
+        $('#gallery').append(div);
+
+    });
+
 });
 
+// async function to read and return the json file
 async function getJSON() {
     return await fetch(path)
-        .then((response)=>response.json())
-        .then((responseJson)=>{return responseJson});
+        .then((response) => response.json())
+        .then((responseJson) => { return responseJson });
 }
